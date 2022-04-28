a8acc12e0c69d0dc8594c00c5fcd8e8296730478
hussamkhatib <hussamkhatib20@gmail.com>
2022-04-28 23:19:55 +0530

    use yarn as package manager , delete package.lock..

--- components/Layout.js
+++ components/Layout.js
@@ -125,13 +125,13 @@ function Layout({ children }) {
       </Head>
       <div className="bg-white">
         <header>
-          <div className="container mx-auto px-6 py-3">
+          <div className="container px-6 py-3 mx-auto">
             <div className="flex items-center justify-between">
               <address className="hidden w-full text-gray-600 md:flex md:items-center">
                 <Location />
                 <span className="mx-1 text-sm">BLR</span>
               </address>
-              <h1 className="hidden w-full text-gray-700 md:text-center text-2xl font-semibold">
+              <h1 className="hidden w-full text-2xl font-semibold text-gray-700 md:text-center">
                 Evolution
               </h1>
               <Image
@@ -146,7 +146,7 @@ function Layout({ children }) {
                 </div>
                 {!userAuthState.logIn && (
                   <button
-                    className="bg-blue-500 hover:bg-blue-300 text-white p-2"
+                    className="p-2 text-white bg-blue-500 hover:bg-blue-300"
                     onClick={() => dispatch(toggleForm())}
                   >
                     Sign Up
@@ -154,7 +154,7 @@ function Layout({ children }) {
                 )}
                 <button
                   onClick={() => dispatch(openSliderComponent("Cart"))}
-                  className="text-gray-600 text-xs focus:outline-none px-4"
+                  className="px-4 text-xs text-gray-600 focus:outline-none"
                 >
                   <CartIcon />
                   Cart
@@ -224,22 +224,6 @@ function Layout({ children }) {
                 </Link>
               </ul>
             </nav>
-            {/* <form role='search' className="relative mt-6 max-w-lg mx-auto">
-            <button type='submit'>
-            <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
-              <SearchIcon />
-            </span>
-            </button>
-            <label for="products-input"><span className='hidden'>Search</span></label>
-            <input
-              className="w-full border rounded-md pl-10 pr-4 py-2 focus:border-blue-500 focus:outline-none focus:shadow-outline"
-              type="text"
-              title='Search'
-              autocomplete='off'
-              placeholder="Search"
-            />
-          
-          </form>  */}
           </div>
         </header>
 
@@ -255,12 +239,12 @@ function Layout({ children }) {
         {renderForm && (
           <>
             <SignupContainer />
-            <div className="fixed opacity-66 bg-black inset-0 overflow-y-auto"></div>
+            <div className="fixed inset-0 overflow-y-auto bg-black opacity-66"></div>
           </>
         )}
 
         <footer className="bg-gray-200">
-          <div className="container mx-auto px-6 py-3 flex justify-between items-center">
+          <div className="container flex items-center justify-between px-6 py-3 mx-auto">
             <a
               href="#"
               className="text-xl font-bold text-gray-500 hover:text-gray-400"
